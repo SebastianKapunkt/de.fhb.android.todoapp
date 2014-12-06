f5f903b4d03ad8b7ea04e2d5245bff4cccf1cae9
Visores <pewpeweule@gmail.com>
2014-12-06 11:48:05 +0100

    add write to database

--- MyTodoApp/src/de/fhb/maus/android/mytodoapp/activities/TodoOverviewActivity.java
+++ MyTodoApp/src/de/fhb/maus/android/mytodoapp/activities/TodoOverviewActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ListView;
 import de.fhb.maus.android.mytodoapp.R;
 import de.fhb.maus.android.mytodoapp.adapter.TodoArrayAdapter;
+import de.fhb.maus.android.mytodoapp.data.Todo;
 import de.fhb.maus.android.mytodoapp.database.MySQLiteHelper;
 
 public class TodoOverviewActivity extends Activity {
@@ -17,22 +18,27 @@ public class TodoOverviewActivity extends Activity {
 		setContentView(R.layout.todo_overview);
 
 		ListView listview = (ListView) findViewById(R.id.todo_list);
-		
-		MySQLiteHelper db = new MySQLiteHelper(this);
 
-		Log.d("Todo", db.getAllTodos().toString());
+		MySQLiteHelper db = new MySQLiteHelper(this);
 		
-		TodoArrayAdapter adapter = new TodoArrayAdapter(this,db.getAllTodos());
+		db.addTodo(new Todo("one","one",false,true,1231232));
+		db.addTodo(new Todo("two","two",true,true,1231232));
+		db.addTodo(new Todo("three","three",true,false,1231232));
+		db.addTodo(new Todo("four","four",true,true,1231232));
 		
-		
+		Log.d("Todos", db.getAllTodos().toString());
+
+		TodoArrayAdapter adapter = new TodoArrayAdapter(this, db.getAllTodos());
+
+		db.close();
 		
 		listview.setAdapter(adapter);
 
 	}
 
-	public void createNewTodo(View view){
+	public void createNewTodo(View view) {
 		Intent intent = new Intent(this, TodoContextActivity.class);
-		
+
 		startActivity(intent);
 	}
 }
