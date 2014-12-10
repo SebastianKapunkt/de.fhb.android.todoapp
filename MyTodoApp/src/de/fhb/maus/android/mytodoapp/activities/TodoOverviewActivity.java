db1a727a4ad6f63b597e0ccf38654200f02e6d06
Visores <pewpeweule@gmail.com>
2014-12-10 20:02:19 +0100

    changed TodoOverview
    extends now ListActivity

--- MyTodoApp/src/de/fhb/maus/android/mytodoapp/activities/TodoOverviewActivity.java
+++ MyTodoApp/src/de/fhb/maus/android/mytodoapp/activities/TodoOverviewActivity.java
@@ -1,6 +1,6 @@
 package de.fhb.maus.android.mytodoapp.activities;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,34 +11,42 @@ import de.fhb.maus.android.mytodoapp.adapter.TodoArrayAdapter;
 import de.fhb.maus.android.mytodoapp.data.Todo;
 import de.fhb.maus.android.mytodoapp.database.MySQLiteHelper;
 
-public class TodoOverviewActivity extends Activity {
+public class TodoOverviewActivity extends ListActivity {
+
+	public static final String SELECTED_TODO = "todo";
 
 	public void onCreate(Bundle saveInstanceState) {
 		super.onCreate(saveInstanceState);
 		setContentView(R.layout.todo_overview);
 
-		ListView listview = (ListView) findViewById(R.id.todo_list);
-
 		MySQLiteHelper db = new MySQLiteHelper(this);
-		
-		db.addTodo(new Todo("one","one",false,true,1231232));
-		db.addTodo(new Todo("two","two",true,true,1231232));
-		db.addTodo(new Todo("three","three",true,false,1231232));
-		db.addTodo(new Todo("four","four",true,true,1231232));
-		
+
+		db.addTodo(new Todo("one", "one", false, true, 1231232));
+		db.addTodo(new Todo("two", "two", true, true, 1231232));
+		db.addTodo(new Todo("three", "three", true, false, 1231232));
+		db.addTodo(new Todo("four", "four", true, true, 1231232));
+
 		Log.d("Todos", db.getAllTodos().toString());
+		db.close();
 
 		TodoArrayAdapter adapter = new TodoArrayAdapter(this, db.getAllTodos());
-
-		db.close();
-		
-		listview.setAdapter(adapter);
+		setListAdapter(adapter);
 
 	}
 
 	public void createNewTodo(View view) {
-		Intent intent = new Intent(this, TodoContextActivity.class);
+		startActivity(new Intent(this, TodoContextActivity.class));
+	}
 
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		// Get the item that was clicked
+		Todo todo = (Todo) this.getListAdapter().getItem(position);
+
+		Intent intent = new Intent(this, TodoContextActivity.class);
+		intent.putExtra(SELECTED_TODO, todo.getId());
 		startActivity(intent);
+
 	}
 }
