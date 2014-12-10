30890e9c898aa492b3ad8e5be120466749e41fe3
Visores <pewpeweule@gmail.com>
2014-12-10 21:51:13 +0100

    new feature -> onItemClick

--- MyTodoApp/src/de/fhb/maus/android/mytodoapp/activities/TodoOverviewActivity.java
+++ MyTodoApp/src/de/fhb/maus/android/mytodoapp/activities/TodoOverviewActivity.java
@@ -1,6 +1,6 @@
 package de.fhb.maus.android.mytodoapp.activities;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,10 +8,9 @@ import android.view.View;
 import android.widget.ListView;
 import de.fhb.maus.android.mytodoapp.R;
 import de.fhb.maus.android.mytodoapp.adapter.TodoArrayAdapter;
-import de.fhb.maus.android.mytodoapp.data.Todo;
 import de.fhb.maus.android.mytodoapp.database.MySQLiteHelper;
 
-public class TodoOverviewActivity extends ListActivity {
+public class TodoOverviewActivity extends Activity {
 
 	public static final String SELECTED_TODO = "todo";
 
@@ -20,33 +19,22 @@ public class TodoOverviewActivity extends ListActivity {
 		setContentView(R.layout.todo_overview);
 
 		MySQLiteHelper db = new MySQLiteHelper(this);
-
-		db.addTodo(new Todo("one", "one", false, true, 1231232));
-		db.addTodo(new Todo("two", "two", true, true, 1231232));
-		db.addTodo(new Todo("three", "three", true, false, 1231232));
-		db.addTodo(new Todo("four", "four", true, true, 1231232));
+		
+//		db.addTodo(new Todo("one", "one", false, true, 1231232));
+//		db.addTodo(new Todo("two", "two", true, true, 1231232));
+//		db.addTodo(new Todo("three", "three", true, false, 1231232));
+//		db.addTodo(new Todo("four", "four", true, true, 1231232));
 
 		Log.d("Todos", db.getAllTodos().toString());
 		db.close();
 
+		ListView list = (ListView) findViewById(R.id.todo_list);
 		TodoArrayAdapter adapter = new TodoArrayAdapter(this, db.getAllTodos());
-		setListAdapter(adapter);
+		list.setAdapter(adapter);
 
 	}
 
 	public void createNewTodo(View view) {
 		startActivity(new Intent(this, TodoContextActivity.class));
-	}
-
-	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		super.onListItemClick(l, v, position, id);
-		// Get the item that was clicked
-		Todo todo = (Todo) this.getListAdapter().getItem(position);
-
-		Intent intent = new Intent(this, TodoContextActivity.class);
-		intent.putExtra(SELECTED_TODO, todo.getId());
-		startActivity(intent);
-
 	}
 }
--- MyTodoApp/src/de/fhb/maus/android/mytodoapp/adapter/TodoArrayAdapter.java
+++ MyTodoApp/src/de/fhb/maus/android/mytodoapp/adapter/TodoArrayAdapter.java
@@ -3,25 +3,31 @@ package de.fhb.maus.android.mytodoapp.adapter;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
-import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import de.fhb.maus.android.mytodoapp.R;
+import de.fhb.maus.android.mytodoapp.activities.TodoContextActivity;
 import de.fhb.maus.android.mytodoapp.data.Todo;
 
 public class TodoArrayAdapter extends ArrayAdapter<Todo> {
 
+	protected static final String SELECTED_TODO = "todo";
 	private final Activity context;
 	private final ArrayList<Todo> todos;
 
 	static class ViewHolder {
 		private TextView todoName;
-		private ImageButton isImportant;
+		private ImageView isImportant;
 		private CheckBox isDoneCheckbox;
+		private LinearLayout linearLayout;
 	}
 
 	public TodoArrayAdapter(Activity context, ArrayList<Todo> todos) {
@@ -31,25 +37,38 @@ public class TodoArrayAdapter extends ArrayAdapter<Todo> {
 	}
 
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
+	public View getView(final int position, View convertView, ViewGroup parent) {
 		View rowView = convertView;
 
 		if (rowView == null) {
 			LayoutInflater inflater = (LayoutInflater) context
 					.getLayoutInflater();
 			rowView = inflater.inflate(R.layout.rowlayout, parent, false);
-			
+
 			ViewHolder holder = new ViewHolder();
 			holder.todoName = (TextView) rowView.findViewById(R.id.todo_name);
 			holder.isDoneCheckbox = (CheckBox) rowView
 					.findViewById(R.id.isdone);
-			holder.isImportant = (ImageButton) rowView
+			holder.isImportant = (ImageView) rowView
 					.findViewById(R.id.isImportant);
+			holder.linearLayout = (LinearLayout) rowView
+					.findViewById(R.id.row_layout);
 			rowView.setTag(holder);
 		}
-		
+
 		ViewHolder holder = (ViewHolder) rowView.getTag();
 		holder.todoName.setText(todos.get(position).getName());
+
+		holder.linearLayout.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(context, TodoContextActivity.class);
+				intent.putExtra(SELECTED_TODO, todos.get(position).getId());
+				context.startActivity(intent);
+			}
+		});
+
 		holder.isDoneCheckbox.setChecked(todos.get(position).isDone());
 		if (todos.get(position).isImportant()) {
 			holder.isImportant.setImageResource(R.drawable.is_fav);
