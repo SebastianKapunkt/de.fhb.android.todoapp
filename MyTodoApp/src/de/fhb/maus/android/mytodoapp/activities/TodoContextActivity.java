66c1345b3ca2f870147e18380402088fdf2aa7ca
Visores <pewpeweule@gmail.com>
2014-12-18 17:31:09 +0100

    edit Context view
    item can now be edit create or delete

--- MyTodoApp/src/de/fhb/maus/android/mytodoapp/activities/TodoContextActivity.java
+++ MyTodoApp/src/de/fhb/maus/android/mytodoapp/activities/TodoContextActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,34 +12,81 @@ import de.fhb.maus.android.mytodoapp.R;
 import de.fhb.maus.android.mytodoapp.data.Todo;
 import de.fhb.maus.android.mytodoapp.database.MySQLiteHelper;
 
-public class TodoContextActivity extends Activity{
+public class TodoContextActivity extends Activity {
 
 	private Todo todo;
-	
+	private ViewHolder holder = new ViewHolder();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.todo_context);
 		Intent intent = getIntent();
 		long id = intent.getLongExtra("todo", -1);
-		Log.d("Get Intent", id+"");
-		
-		if(id != -1){
+		Log.d("Get Intent", id + "");
+
+		holder.todoname = (EditText) findViewById(R.id.todo_edit_name);
+		holder.tododesc = (EditText) findViewById(R.id.todo_edit_desc);
+		holder.isDone = (CheckBox) findViewById(R.id.isDone_context);
+		holder.isImportant = (CheckBox) findViewById(R.id.isImportant_context);
+		holder.datetime = (TextView) findViewById(R.id.datetime);
+
+		if (id != -1) {
 			MySQLiteHelper db = new MySQLiteHelper(this);
 			todo = db.getTodo(id);
 			db.close();
-			
-			EditText todoname = (EditText) findViewById(R.id.todo_edit_name);
-			todoname.setText(todo.getName());
-			EditText tododesc = (EditText) findViewById(R.id.todo_edit_desc);
-			tododesc.setText(todo.getDescription());
-			CheckBox isDone = (CheckBox) findViewById(R.id.isDone_context);
-			isDone.setChecked(todo.isDone());
-			CheckBox isImportant = (CheckBox) findViewById(R.id.isImportant_context);
-			isImportant.setChecked(todo.isImportant());
-			TextView datetime = (TextView) findViewById(R.id.datetime);
-			datetime.setText(todo.convertTime(todo.getMaturityDate()));
+			holder.todoname.setText(todo.getName());
+			holder.tododesc.setText(todo.getDescription());
+			holder.isDone.setChecked(todo.isDone());
+			holder.isImportant.setChecked(todo.isImportant());
+			holder.datetime.setText(todo.convertTime(todo.getMaturityDate()));
+		} else {
+			todo = new Todo();
+			todo.setId(-1);
 		}
 	}
-	
+
+	public void saveTodoItem(View v) {
+		MySQLiteHelper db = new MySQLiteHelper(v.getContext());
+
+		Log.d("Todo", todo.toString());
+
+		if (todo.getId() != -1) {
+			todo.setName(holder.todoname.getText().toString());
+			todo.setDescription(holder.tododesc.getText().toString());
+			todo.setDone(holder.isDone.isChecked());
+			todo.setImportant(holder.isImportant.isChecked());
+			todo.setMaturityDate(todo.convertTime(holder.datetime.getText()
+					.toString()));
+			db.updateTodo(todo);
+		} else {
+			db.addTodo(todo);
+		}
+
+		db.close();
+
+		startActivity(new Intent(this, TodoOverviewActivity.class));
+	}
+
+	public void deleteTodoItem(View v) {
+		MySQLiteHelper db = new MySQLiteHelper(v.getContext());
+
+		Log.d("Todo", todo.toString());
+
+		if (todo.getId() != -1) {
+			db.deleteTodo(todo);
+		}
+
+		db.close();
+
+		startActivity(new Intent(this, TodoOverviewActivity.class));
+	}
+
+	static class ViewHolder {
+		private EditText todoname;
+		private EditText tododesc;
+		private CheckBox isDone;
+		private CheckBox isImportant;
+		private TextView datetime;
+	}
 }
