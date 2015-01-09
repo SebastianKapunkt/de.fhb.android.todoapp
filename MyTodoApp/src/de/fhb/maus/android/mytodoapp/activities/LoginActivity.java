79cf7fddab781bbec6f448a7efed89e67bd70627
Visores <pewpeweule@gmail.com>
2015-01-09 16:34:06 +0100

    Edit LoginActivity
    login Button only enabled when text is in password and email field
    error message for the cases

--- MyTodoApp/src/de/fhb/maus/android/mytodoapp/activities/LoginActivity.java
+++ MyTodoApp/src/de/fhb/maus/android/mytodoapp/activities/LoginActivity.java
@@ -1,17 +1,39 @@
 package de.fhb.maus.android.mytodoapp.activities;
 
-import de.fhb.maus.android.mytodoapp.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import de.fhb.maus.android.mytodoapp.R;
 
 public class LoginActivity extends Activity {
+
+	private EditText email;
+	private EditText password;
+	private TextView hintEmail;
+	private TextView hintPassword;
+	private Button login;
+	boolean passwordlenght;
+	boolean emaillenght;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+
+		email = (EditText) findViewById(R.id.email_address);
+		password = (EditText) findViewById(R.id.password);
+		login = (Button) findViewById(R.id.login_button);
+		hintEmail = (TextView) findViewById(R.id.hintemail);
+		hintEmail.setText("no Email set");
+		hintPassword = (TextView) findViewById(R.id.hintpassword);
+		hintPassword.setText("no password set");
+		login.setEnabled(false);
 
 		boolean isServerAvaible = true;
 
@@ -21,6 +43,70 @@ public class LoginActivity extends Activity {
 			logIn(getCurrentFocus());
 		}
 
+		email.addTextChangedListener(new TextWatcher() {
+
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+				if (s.length() > 0) {
+					hintEmail.setText("");
+					emaillenght = true;
+					if (passwordlenght) {
+						login.setEnabled(true);
+					} else {
+						login.setEnabled(false);
+					}
+				} else {
+					hintEmail.setText("no Email set");
+					emaillenght = false;
+				}
+			}
+
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+			}
+
+			public void afterTextChanged(Editable s) {
+				for (int i = s.length(); i > 0; i--) {
+
+					if (s.subSequence(i - 1, i).toString().equals("\n"))
+						s.replace(i - 1, i, "");
+
+				}
+			}
+		});
+
+		password.addTextChangedListener(new TextWatcher() {
+
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+				if (s.length() > 0) {
+					passwordlenght = true;
+					hintPassword.setText("");
+					if (emaillenght) {
+						login.setEnabled(true);
+					} else {
+						login.setEnabled(false);
+					}
+
+				} else {
+					hintPassword.setText("no Password set");
+					passwordlenght = false;
+				}
+			}
+
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+			}
+
+			public void afterTextChanged(Editable s) {
+				for (int i = s.length(); i > 0; i--) {
+
+					if (s.subSequence(i - 1, i).toString().equals("\n"))
+						s.replace(i - 1, i, "");
+
+				}
+			}
+		});
 	}
 
 	public void logIn(View view) {
