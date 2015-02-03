7f3351c269dc3e742f8c8c61cfc2503d0b7b31d4
mrep <der_kakerlak@web.de>
2015-02-03 15:44:08 +0100

    Location-Update #2: LocationActivity

---
+++ MyTodoApp/src/de/fhb/maus/android/mytodoapp/activities/LocationActivity.java
@@ -0,0 +1,168 @@
+package de.fhb.maus.android.mytodoapp.activities;
+
+import java.io.IOException;
+import java.util.List;
+
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.app.FragmentManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import de.fhb.maus.android.mytodoapp.R;
+
+public class LocationActivity extends FragmentActivity 
+								implements OnMapReadyCallback, OnMarkerDragListener {
+	
+	private MarkerOptions mOptions;
+	private Marker marker;
+	private GoogleMap map;
+	EditText locInfo;
+	private static final String TAG = "LocationAddress";
+	String geoCodeResult = "";
+	String locationName = "";
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.location);
+		Intent intent = getIntent();
+		
+		locInfo = (EditText)findViewById(R.id.locinfo);
+		if(intent.hasExtra("locationName")) {
+			locInfo.setText(intent.getStringExtra("locationName"));
+		}
+		locInfo.addTextChangedListener(new TextWatcher() {
+
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+
+			}
+
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+
+			}
+
+			public void afterTextChanged(Editable s) {
+				locationName = locInfo.getText().toString();
+			}
+		});
+		
+		mOptions = new MarkerOptions()
+			.position(new LatLng(intent.getDoubleExtra("locationLatitude", 52.41192),
+									intent.getDoubleExtra("locationLongitude", 12.53126)))
+			.draggable(true);
+		
+		FragmentManager fManager = getFragmentManager();
+		MapFragment mapFragment = (MapFragment) fManager.findFragmentById(R.id.map); //R.id.map
+		map = mapFragment.getMap();
+		mapFragment.getMapAsync(this);
+		
+		GoogleMapOptions options = new GoogleMapOptions();
+		options.mapType(GoogleMap.MAP_TYPE_NORMAL)
+						.compassEnabled(false)
+						.rotateGesturesEnabled(false)
+						.tiltGesturesEnabled(false);
+		MapFragment.newInstance(options);
+		map.setMapType(GoogleMap.MAP_TYPE_NORMAL);		
+		map.setOnMarkerDragListener(this);
+		
+		marker = map.addMarker(mOptions);
+	}
+	
+	@Override
+	public void onMapReady(GoogleMap map) {		
+		//marker = map.addMarker(mOptions);
+	}
+	
+	/**
+	 * Fuegt dem Intent Koordinaten und Location-Name hinzu und beendet die Activity.
+	 * 
+	 * @param v
+	 */
+	public void saveLocationChange(View v) {
+		Intent returnIntent = new Intent();
+		
+		geoCode();
+		
+		returnIntent.putExtra("locationName", locationName);
+		returnIntent.putExtra("locationLatitude", marker.getPosition().latitude);
+		returnIntent.putExtra("locationLongitude", marker.getPosition().longitude);
+		setResult(RESULT_OK, returnIntent);
+		finish();
+	}
+	
+	/**
+	 * Bestimmt aus den Koordinaten des Markers die entsprechende Adresse.
+	 */
+	public void geoCode() {
+		Geocoder gCoder = new Geocoder(this);
+		
+		try {
+			List<Address> addressList = gCoder.getFromLocation(
+										marker.getPosition().latitude, 
+										marker.getPosition().longitude,
+										1);
+			if(addressList != null && addressList.size() > 0) {
+				Address address = addressList.get(0);
+				StringBuilder sb = new StringBuilder();
+				for(int i = 0; i < address.getMaxAddressLineIndex(); i++) {
+					sb.append(address.getAddressLine(i)).append("\n");
+				}
+				sb.append(address.getLocality()).append("\n");
+				sb.append(address.getPostalCode()).append("\n");
+				sb.append(address.getCountryName());
+				geoCodeResult = sb.toString();
+				locationName = geoCodeResult;
+			}
+		}
+		catch(IOException e) {
+			Log.e(TAG, "Unable to connect to Geocoder", e);
+		}
+	}
+	
+	/**
+	 * Bricht das Bearbeiten der Location ab.
+	 * 
+	 * @param v
+	 */
+	public void cancelLocationChange(View v) {
+		Intent returnIntent = new Intent();
+		setResult(RESULT_CANCELED, returnIntent);
+		finish();
+	}
+
+	@Override
+	public void onMarkerDrag(Marker marker) {
+		//locInfo.setText(locationName);
+		//locInfo.setText("Marker at: " + marker.getPosition().toString() + " [DragStart]");
+	}
+
+	@Override
+	public void onMarkerDragEnd(Marker marker) {
+		geoCode();
+		locInfo.setText(locationName);
+		
+	}
+
+	@Override
+	public void onMarkerDragStart(Marker marker) {
+		//locInfo.setText(locationName);	
+	}
+}
---
+++ MyTodoApp/src/de/fhb/maus/android/mytodoapp/activities/TodoLocationOverviewActivity.java
@@ -0,0 +1,5 @@
+package de.fhb.maus.android.mytodoapp.activities;
+
+public class TodoLocationOverviewActivity {
+
+}
