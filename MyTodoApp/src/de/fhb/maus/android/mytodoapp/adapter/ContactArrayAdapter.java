fb43f1c79c7d8155a177be30ac17d908192abea0
Daniel Weis <d.weis.92@gmail.com>
2015-02-03 19:20:02 +0100

    Fixed wrong contact pictures

--- MyTodoApp/src/de/fhb/maus/android/mytodoapp/adapter/ContactArrayAdapter.java
+++ MyTodoApp/src/de/fhb/maus/android/mytodoapp/adapter/ContactArrayAdapter.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.content.ContentUris;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.Contacts;
@@ -22,6 +23,7 @@ public class ContactArrayAdapter extends ArrayAdapter<Contact>{
 	
 	private final Activity context;
 	private final ArrayList<Contact> contacts;
+	private Drawable contactPlaceholder;
 	
 	// ViewHolder for viewholder pattern from listview
 	static class ViewHolder {
@@ -33,6 +35,7 @@ public class ContactArrayAdapter extends ArrayAdapter<Contact>{
 		super(context, R.layout.todo_by_c_contacts_rowlayout, contacts);
 		this.context = context;
 		this.contacts = contacts;
+		contactPlaceholder = context.getResources().getDrawable(R.drawable.contact_placeholder);
 	}
 	
 	@Override
@@ -56,11 +59,13 @@ public class ContactArrayAdapter extends ArrayAdapter<Contact>{
 		// get the holder applied to the tag
 		final ViewHolder holder = (ViewHolder) rowView.getTag();
 		
-		//TODO: Set contact picture
 		Uri contactUri = ContentUris.withAppendedId(Contacts.CONTENT_URI, contacts.get(position).getId());
 		InputStream photo = ContactsContract.Contacts.openContactPhotoInputStream(getContext().getContentResolver(), contactUri);
 		if (photo != null)
 			holder.contactPicture.setImageBitmap(BitmapFactory.decodeStream(photo));
+		else{
+			holder.contactPicture.setImageDrawable(contactPlaceholder);
+		}
 		holder.contactName.setText(contacts.get(position).getName());
 		
 		return rowView;
